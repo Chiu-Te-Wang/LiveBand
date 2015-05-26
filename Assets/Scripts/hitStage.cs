fc494cefc1c4dc6d62a0c739482e070f9e9e8bfa
Chiu-Te-Wang <qwas65166516@gmail.com>
2015-05-26 16:05:30 +0800

    first commit

---
+++ Assets/Scripts/hitStage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitStage : MonoBehaviour {
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Stage") {
+			Debug.Log ("hit stage!!!");
+			transform.position = new Vector3(-0.65f,1.7f,4.15f);
+			transform.rotation = new Quaternion(0f,0f,0f,0f);
+		}
+	}
+}
---
+++ Assets/Scripts/spotlightMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spotlightMove : MonoBehaviour {
+
+	// Use this for initialization
+	public float tumble = 10f;
+	public float speed = 0.005f;
+	void Start () {
+		InvokeRepeating("rotateSpotlight", 0, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {  
+
+	}
+
+	void rotateSpotlight(){
+		float xRotation = transform.rotation.x + Random.Range(-1.0F, 1.0F) * tumble;
+		float yRotation = transform.rotation.y + Random.Range(-1.0F, 1.0F) * tumble;
+		float zRotation = transform.rotation.z + Random.Range(-1.0F, 1.0F) * tumble;
+		//if (zRotation < 267f/360f) { zRotation = 267f/360f; }
+		//if (zRotation > 60f/360f) { zRotation = 60f/360f; }
+		//if (xRotation < -125f/360f) { zRotation = -125f/360f; }
+		//if (xRotation > 60f/360f) { zRotation = 60f/360f; }
+		Quaternion newRotation = new Quaternion (xRotation, yRotation, zRotation,0f);
+		transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.time*speed);
+	}
+}
