ca4ce632459837240102865846ab7f4d05ef6470
davidhu34 <b01901133@gmail.com>
2015-05-30 13:11:27 +0800

    add piano, drum

---
+++ Assets/instruments/scripts/keypress.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keypress : MonoBehaviour {
+	private bool pressing;
+	// Use this for initialization
+	void Start () {	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown () {
+		//GetComponent<AudioSource>().Play ();
+	}
+	void OnMouseUp (){
+		GetComponent<AudioSource> ().Stop ();
+		GetComponent<Transform> ().Translate(0,0.09f,0) ;
+	}
+	public void PP() {
+		GetComponent<AudioSource> ().Play ();
+		GetComponent<Transform> ().Translate(0,-0.09f,0) ;
+	}
+}
+
---
+++ Assets/instruments/scripts/raycastcollide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class raycastcollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Input.GetMouseButtonDown(0)) {
+
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit)) {
+					if (hit.collider != null)
+						hit.collider.gameObject.GetComponent<keypress>().PP();
+				}
+			}
+	}
+}
+
