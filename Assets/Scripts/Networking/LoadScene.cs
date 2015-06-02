b99c12a0704b7892aca4e195bf5940dac611820c
Chen-Yan-Ting <deityeater@gmail.com>
2015-06-03 02:58:32 +0800

    upload loadscene.cs

---
+++ Assets/Scripts/Networking/LoadScene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : Photon.MonoBehaviour {
+
+	public void join(){
+		Debug.Log ("join");
+		string s = PlayerPrefs.GetString ("Create_RoomName");
+		string ss = PlayerPrefs.GetString ("Join_RoomName");
+
+		if (s != null) {
+			PhotonNetwork.CreateRoom(s, new RoomOptions(){maxPlayers = 4}, null);
+		}
+		else if(ss != null){
+			PhotonNetwork.JoinRoom(ss);
+		}
+	}
+
+	public void OnJoinedRoom()
+	{
+		Debug.Log("OnJoinedRoom");
+		PhotonNetwork.LoadLevel("_scene");
+	}
+	
+	public void OnCreatedRoom()
+	{
+		Debug.Log("OnCreatedRoom");
+		PhotonNetwork.LoadLevel("_scene");
+	}
+}
--- Assets/instruments/scripts/drumpress.cs
+++ Assets/instruments/scripts/drumpress.cs
@@ -54,4 +54,5 @@ public class drumpress : MonoBehaviour {
 		GetComponent<AudioSource> ().Play ();
 
 	}
+
 }
