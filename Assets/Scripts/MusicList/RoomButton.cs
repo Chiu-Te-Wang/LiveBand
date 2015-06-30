9124585b700be41c78ee6b7cfc2a1f15b8c76271
Chen-Yan-Ting <deityeater@gmail.com>
2015-07-01 05:32:15 +0800

    improving UI

---
+++ Assets/Scripts/MusicList/RoomButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class RoomButton : MonoBehaviour {
+
+	public Button button;
+	public Text nameLabel;
+	public Button j;
+
+	void Awake(){
+		GameObject playerInput2 = GameObject.Find ("RoomCanvas");
+		Button[] c = playerInput2.GetComponentsInChildren<Button> ();
+		Debug.Log ("c: " + c.Length);
+		j = c [1];
+	}
+	public void destroythis(){
+		Destroy (this.gameObject);
+	}
+
+	public void choose(){
+		PlayerPrefs.SetString("Join_RoomName", nameLabel.text);
+		j.interactable = true;
+	}
+}
