28d55f3a0a06434ebbc3e18e90be769b77eb5bdb
Chen-Yan-Ting <deityeater@gmail.com>
2015-07-02 03:27:42 +0800

    syn

--- Assets/instruments/scripts/synthTouch.cs
+++ Assets/instruments/scripts/synthTouch.cs
@@ -9,35 +9,44 @@ public class synthTouch : Photon.MonoBehaviour {
 	private string chordNew;
 	private string chordOld;
 
+	public Text fuckBug;
 
 
 	private Transform trans;
-	public AudioSource[] sounds = new AudioSource[39];
+
 	public string chord_pressed; 
 
+	private AudioSource[] result;
 
+
+
+	void Awake(){
+		result = new AudioSource[5];
+	}
 
 	// Use this for initialization
 	void Start () {
 		trans = GetComponent<Transform>();
-		touchOld = GetComponent<GameObject> ();
 		chordOld = "";
+		touchOld = null;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
+		touchNew = null;
 		foreach ( Touch touch in Input.touches ) {
-
+			fuckBug.text = "Got touches!";
 			Ray ray = Camera.main.ScreenPointToRay(touch.position);
 			RaycastHit hit;
 			if ( Physics.Raycast(ray, out hit, Mathf.Infinity, 1<<11 | 1<<5)) {
 				if (hit.transform.gameObject.layer == LayerMask.NameToLayer("SYNTHESIZERTOUCH")) {
 					if (hit.collider != null) {
 						GameObject key = hit.collider.gameObject;
-
+						fuckBug.text = "Got hit!";
 						if ( touch.phase != TouchPhase.Ended
 					  	&& touch.phase != TouchPhase.Canceled ) {
+							fuckBug.text = "in";
 							touchNew = key;
 							chordNew = chord_pressed;
 							break;
@@ -45,56 +54,21 @@ public class synthTouch : Photon.MonoBehaviour {
 					}
 				}
 			}
-		} if (touchNew != touchOld) {
-			touchOld.GetComponent<synthpress>().Up( GetComponent<recorder>().getBool());
-			touchNew.GetComponent<synthpress>().PP ( GetComponent<recorder>().getBool(), Chord(touchNew.name, chord_pressed));
+		}
+
+		if (touchNew != touchOld) {
+			if ( touchOld != null) {
+				touchOld.GetComponent<synthpress>().Up();
+				//touchOld.transform.Translate (0, 0.09f, 0);
+			}
+			if ( touchNew != null ){
+				fuckBug.text = "touch name: "+ touchNew.name;
+				touchNew.GetComponent<synthpress>().PP ( touchNew.name, chord_pressed);
+				//touchNew.transform.Translate(0, -0.09f,0);
+			}
 		}
 		touchOld = touchNew;
 		chordOld = chordNew;
 	}
 
-	AudioSource[] Chord ( string bas, string chord ) {
-		int[] notes = new int[5];
-		int tmp = 0;
-		if ( bas == "C" )	tmp = 12;
-		else if ( bas == "D" )	tmp = 13;
-		else if ( bas == "E" ) 	tmp = 14;
-		else if ( bas == "F" ) 	tmp = 15;
-		else if ( bas == "G" ) 	tmp = 16;
-		else if ( bas == "A" ) 	tmp = 10;
-		else if ( bas == "B" )	tmp = 11;
-
-		for ( int i = 0; i < 5; i++ )	i = tmp;
-
-		if (chord == "M") {
-			notes[1] += 4; notes[2] += 7;
-		} else if ( chord == "M7" ) {
-			notes[1] += 4; notes[2] += 7; notes[3] += 11;
-		} else if ( chord == "m" ) {
-			notes[1] += 3; notes[2] += 7;
-		} else if ( chord == "m7" ) {
-			notes[1] += 3; notes[2] += 7; notes[3] += 10;
-		} else if ( chord == "7" ) {
-			notes[1] += 4; notes[2] += 7; notes[3] += 10;
-		} else if ( chord == "6" ) {
-			notes[1] += 4; notes[2] += 7; notes[3] += 9;
-		} else if ( chord == "sus4" ) {
-			notes[1] += 5; notes[2] += 7;
-		} else if ( chord == "dim" ) {
-			notes[1] += 3; notes[2] += 6; notes[3] += 9;
-		} else if ( chord == "aug" ) {
-			notes[1] += 4; notes[2] += 8;
-		} else if ( chord == "add9" ) {
-			notes[1] += 2; notes[2] += 4; notes[3] += 7;
-		}
-
-		if (notes [3] == notes [0]) {
-			notes [3] += 12;
-			notes [4] = notes [1] + 12;
-		} else notes[4] = notes[0] + 12;
-
-		AudioSource[] result = new AudioSource[5];
-		for ( int i = 0; i < 5; i++ )	result[i] = sounds[ notes[i] ];
-		return result;
-	}
 }
--- Assets/instruments/scripts/synthpress.cs
+++ Assets/instruments/scripts/synthpress.cs
@@ -8,8 +8,14 @@ public class synthpress : Photon.MonoBehaviour {
 	public int bpm;
 	public string play_style;
 	private AudioSource[] playing;
-	private float[] delay;	
+	private float[] delay;
 
+	public AudioSource[] sounds;
+	private AudioSource[] result;
+
+	void Awake(){
+		result = new AudioSource[5];
+	}
 	// Use this for initialization
 	void Start () {	
 	}
@@ -22,26 +28,27 @@ public class synthpress : Photon.MonoBehaviour {
 		//GetComponent<AudioSource>().Play ();
 	}
 
-	public void Up ( bool rec){
+	public void Up (){
 		photonView.RPC ("UpRPC", PhotonTargets.AllViaServer);
 		PhotonNetwork.SendOutgoingCommands ();
-		CancelInvoke ();
-		if ( rec )	recEnd = Time.time;
+		//CancelInvoke ();
+
+		//if ( rec )	recEnd = Time.time;
 		//GetComponent<AudioSource> ().Stop ();
 		GetComponent<Transform> ().Translate (0, 0.09f, 0);
-		GetComponentInParent<recorder> ().Add ( GetComponent<AudioSource> (), recStart, recEnd);
+		//GetComponentInParent<recorder> ().Add ( GetComponent<AudioSource> (), recStart, recEnd);
 		recStart = 0;
 		recEnd = 0;
 	}
 
-	public void PP( bool rec, AudioSource[] chord ) {
-		playing = chord;
+	public void PP(  string nameD, string pressed ) {
 
-		PlayChord ();
 
-		photonView.RPC ("PPRPC", PhotonTargets.AllViaServer);
+		//PlayChord ();
+
+		photonView.RPC ("PPRPCS", PhotonTargets.AllViaServer, nameD, pressed);
 		PhotonNetwork.SendOutgoingCommands ();
-		if ( rec ) recStart = Time.time;
+		//if ( rec ) recStart = Time.time;
 
 
 		GetComponent<Transform> ().Translate (0, -0.09f, 0);
@@ -89,15 +96,69 @@ public class synthpress : Photon.MonoBehaviour {
 	void strike4(){ playing [4].Play (); }
 	void strike123(){ playing[1].Play (); playing[2].Play (); playing[3].Play (); }
 
+	AudioSource[] Chord ( string bas, string chord ) {
+		int[] notes = new int[5];
+		int tmp = 0;
+		if ( bas == "C" )	tmp = 17;
+		else if ( bas == "C#" )	tmp = 18;
+		else if ( bas == "D" )	tmp = 19;
+		else if ( bas == "D#" )	tmp = 20;
+		else if ( bas == "E" ) 	tmp = 21;
+		else if ( bas == "F" ) 	tmp = 10;
+		else if ( bas == "G" ) 	tmp = 12;
+		else if ( bas == "A" ) 	tmp = 14;
+		else if ( bas == "F#" ) 	tmp = 11;
+		else if ( bas == "G#" ) 	tmp = 13;
+		else if ( bas == "A#" ) 	tmp = 15;
+		else if ( bas == "B" )	tmp = 16;
+		
+		for ( int i = 0; i < 5; i++ )	notes[i] = tmp;
+		
+		if (chord == "M") {
+			notes[1] += 4; notes[2] += 7;
+		} else if ( chord == "M7" ) {
+			notes[1] += 4; notes[2] += 7; notes[3] += 11;
+		} else if ( chord == "m" ) {
+			notes[1] += 3; notes[2] += 7;
+		} else if ( chord == "m7" ) {
+			notes[1] += 3; notes[2] += 7; notes[3] += 10;
+		} else if ( chord == "7" ) {
+			notes[1] += 4; notes[2] += 7; notes[3] += 10;
+		} else if ( chord == "6" ) {
+			notes[1] += 4; notes[2] += 7; notes[3] += 9;
+		} else if ( chord == "sus4" ) {
+			notes[1] += 5; notes[2] += 7;
+		} else if ( chord == "dim" ) {
+			notes[1] += 3; notes[2] += 6; notes[3] += 9;
+		} else if ( chord == "aug" ) {
+			notes[1] += 4; notes[2] += 8;
+		} else if ( chord == "add9" ) {
+			notes[1] += 2; notes[2] += 4; notes[3] += 7;
+		}
+		
+		if (notes [3] == notes [0]) {
+			notes [3] += 12;
+			notes [4] = notes [1] + 12;
+		} else notes[4] = notes[0] + 12;
+		
+		result = new AudioSource[5];
+		for ( int i = 0; i < 5; i++ )	
+			result[i] = sounds[ notes[i] ];
+		return result;
+	}
+
+
+
 
 	[RPC]
 	void UpRPC(){
-		GetComponent<AudioSource> ().Stop ();
+		CancelInvoke ();
 	}
 
 	[RPC]
-	void PPRPC(){
-		GetComponent<AudioSource> ().Play ();
+	void PPRPCS(string n, string pressed){
+		playing = Chord( n, pressed);
+		PlayChord ();
 		print ("ssss");
 	}
 }
